91d62b28683e187d63b35c41d0cb8ed0c40b96cd
Reshawn <reshawnhamilton97@gmail.com>
2022-01-14 06:43:58 +0000

    checkout items can be dynamically added

--- routes/checkout.js
+++ routes/checkout.js
@@ -6,16 +6,16 @@ module.exports = (db) => {
 
   router.get("/", (req, res) => {      // non-AJAX route; returns full page of HTML
 
-    const templateVars = {
-      order: req.session.order
-    };
-    console.log(templateVars);
-    res.render("checkout", templateVars);
-
-    db.query("UPDATE menu_item SET item_quantity = $2 WHERE id = $1;",[Object.keys(templateVars.order[0])[0],Object.values(templateVars.order[0])[0] ])
+    db.query("UPDATE menu_item SET item_quantity = $2 WHERE id = $1;",[Object.keys(req.session.order[0])[0],Object.values(req.session.order[0])[0] ])
     .then(
-      db.query( "SELECT * FROM menu_item WHERE id = $1;",[Object.keys(templateVars.order[0])[0]])
-      .then(data => {console.log(data.rows)})
+      db.query( "SELECT * FROM menu_item WHERE id = $1;",[Object.keys(req.session.order[0])[0]])
+      .then(data => {console.log(data.rows)
+        const templateVars = {
+          order: req.session.order,
+          menu_item: data.rows
+        };
+        console.log(templateVars);
+        res.render("checkout", templateVars);})
       );
 
 
